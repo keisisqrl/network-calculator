8e05110c6544bc2a7c8efa4c1611b67e549915d4
Amy <keisisqrl@thexyin.sexy>
2018-10-05 13:01:45 -0700

    initial checkin, ready to go

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import XferCalc from './XferCalc.js';
+import WindowCalc from './WindowCalc.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <XferCalc />
+        <WindowCalc />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/XferCalc.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './form.css';
+var humanFormat = require('human-format');
+
+class XferCalc extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      "rate": 100000000, // 100 Mbit/s
+      "size": 8230000 // 8.23 Mbyte
+    };
+  }
+
+  changeState = e => {
+    if (isNaN(parseFloat(e.target.value))) {
+      this.setState({
+        [e.target.name]: 0
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+  }
+
+  render (){
+    const { rate,size } = this.state;
+    const xferTime = parseInt(size) * 8 / parseInt(rate);
+
+    return (
+      <form className="calc">
+        <fieldset>
+          <legend>Calculate transfer time for rate</legend>
+          <div>
+            <label htmlFor="rate">Rate in bits per second</label>
+            <input type="text"
+              name="rate"
+              value={rate}
+              onChange={this.changeState}
+              />
+            <span className="value">{
+                  humanFormat(parseInt(rate),{unit: 'b/s'})
+                }</span>
+
+          </div>
+        <div>
+          <label htmlFor="size">Transfer size in bytes</label>
+          <input type="text"
+            name="size"
+            value={size}
+            onChange={this.changeState}
+            />
+            <span className="value">{
+                  humanFormat(parseInt(size),{unit: 'B'})
+                }</span>
+      </div>
+      <div>
+        <label htmlFor="time">Theoretical transfer time</label>
+        <span className="value">{xferTime} s
+        </span>
+        <span className="value">
+          {humanFormat(xferTime,{unit:'s'})}
+        </span>
+      </div>
+      </fieldset>
+      </form>
+    )
+  }
+}
+
+export default XferCalc;
