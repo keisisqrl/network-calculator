871d1968767c0306fb8333e0b2083fb1b8f6c5e9
Amy <keisisqrl@thexyin.sexy>
2018-10-05 14:38:42 -0700

    prettified, refactored to grid layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import logo from './logo.svg';
-import './App.css';
-import XferCalc from './XferCalc.js';
-import WindowCalc from './WindowCalc.js';
+import "./App.css";
+import XferCalc from "./XferCalc.js";
+import WindowCalc from "./WindowCalc.js";
 
 class App extends Component {
   render() {
--- src/WindowCalc.js
+++ src/WindowCalc.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './form.css';
-var humanFormat = require('human-format');
+import React, { Component } from "react";
+import "./form.css";
+var humanFormat = require("human-format");
 
 class WindowCalc extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      "rate": 50000000, // 50 Mbit/s
-      "rtt": 150, // 150 ms
-      "window": 65535 // 65535 bytes
+      rate: 50000000, // 50 Mbit/s
+      rtt: 150, // 150 ms
+      window: 65535 // 65535 bytes
     };
   }
 
@@ -16,87 +16,80 @@ class WindowCalc extends Component {
     if (isNaN(parseFloat(e.target.value))) {
       this.setState({
         [e.target.name]: 0
-      })
+      });
     } else {
       this.setState({
         [e.target.name]: e.target.value
       });
     }
-  }
+  };
 
-  render (){
-    const { rate,rtt,window } = this.state;
-    const currentRate = parseInt(window) * 8 / parseFloat(rtt) * 1000;
+  render() {
+    const { rate, rtt, window } = this.state;
+    const currentRate = ((parseInt(window) * 8) / parseFloat(rtt)) * 1000;
     const idealWindow = Math.round(
-      parseInt(rate) * parseFloat(rtt) / 1000 / 8
+      (parseInt(rate) * parseFloat(rtt)) / 1000 / 8
     );
 
     return (
-      <form className="calc">
-        <fieldset>
-          <legend>Calculate window requirements</legend>
-          <div>
-            <label htmlFor="rate">Bandwidth in bits per second</label>
-            <input type="text"
-              name="rate"
-              value={rate}
-              onChange={this.changeState}
-              />
-            <span className="value">{
-                humanFormat(parseInt(rate),{unit: 'b/s'})
-              }</span>
+      <fieldset>
+        <legend>Calculate window requirements</legend>
+        <div className="grid">
+          <label htmlFor="rate">Bandwidth in bits per second</label>
+          <input
+            type="text"
+            name="rate"
+            value={rate}
+            onChange={this.changeState}
+          />
+          <span className="value">
+            {humanFormat(parseInt(rate), { unit: "b/s" })}
+          </span>
 
-          </div>
-        <div>
           <label htmlFor="rtt">RTT in milliseconds</label>
-          <input type="text"
+          <input
+            type="text"
             name="rtt"
             value={rtt}
             onChange={this.changeState}
-            />
-            <span className="value">{
-                humanFormat(parseFloat(rtt)/1000,{unit: 's'})
-              }</span>
-      </div>
-      <div>
-        <label htmlFor="window">Window in bytes</label>
-        <input type="text"
-          name="window"
-          value={window}
-          onChange={this.changeState}
           />
-          <span className="value">{
-              humanFormat(parseInt(window),{unit: 'B'})
-            }</span>
-    </div>
-      <div>
-        <span className="label">Theoretical transfer rate</span>
-        <span className="value">{currentRate} bits/s</span>
-          <span className="value">{
-              humanFormat(parseInt(currentRate),{unit: 'b/s'})
-            }</span>
-      </div>
-      <div>
-        <span className="label">Theoretical ideal window</span>
-        <span className="value">
-          {idealWindow} Bytes
-        </span>
-        <span className="value">
-          {humanFormat(idealWindow,{unit: 'B'})}
-        </span>
-      </div>
-      <div>
-        <span className="label">Transfer rate ratio</span>
-        <span className="value">
-          <progress max={rate} value={currentRate} />
+          <span className="value">
+            {humanFormat(parseFloat(rtt) / 1000, { unit: "s" })}
+          </span>
+
+          <label htmlFor="window">Window in bytes</label>
+          <input
+            type="text"
+            name="window"
+            value={window}
+            onChange={this.changeState}
+          />
+          <span className="value">
+            {humanFormat(parseInt(window), { unit: "B" })}
+          </span>
+
+          <span className="label">Theoretical transfer rate</span>
+          <span className="value">{currentRate} bits/s</span>
+          <span className="value">
+            {humanFormat(parseInt(currentRate), { unit: "b/s" })}
+          </span>
+
+          <span className="label">Theoretical ideal window</span>
+          <span className="value">{idealWindow} Bytes</span>
+          <span className="value">
+            {humanFormat(idealWindow, { unit: "B" })}
+          </span>
+
+          <span className="label">Transfer rate ratio</span>
+          <span className="value">
+            <progress max={rate} value={currentRate} />
           </span>
           <span className="value">
-          {Math.round(currentRate / rate * 10000) / 100}%
-        </span>
-      </div>
+            {Math.round((currentRate / rate) * 10000) / 100}%
+          </span>
+        </div>
       </fieldset>
-      </form>
-    )
+    );
   }
 }
 
