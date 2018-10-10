fdd81f0722ff8088eaf132a07aa28080cd0b655f
Amy <keisisqrl@thexyin.sexy>
2018-10-10 11:11:55 -0700

    PWA ready-ish, offline-ready

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import XferCalc from "./XferCalc.js";
 import WindowCalc from "./WindowCalc.js";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      updates: false
+    };
+  }
+
+  notifyUpdates = () => {
+    this.setState({
+      updates: true
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <XferCalc />
         <WindowCalc />
+        { this.state.updates &&
+          <h2 className="updateAlert">Updates ready; please refresh.</h2>
+        }
       </div>
     );
   }
